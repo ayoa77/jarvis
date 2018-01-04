4cdcb28c7722b5495e493b79428f4a881e24ef7a
Alex Frankish <alexfrankish@gmail.com>
2018-01-04 12:09:43 +0800

    scss fix

--- gulpfile.js
+++ gulpfile.js
@@ -5,21 +5,13 @@ var autoprefixer = require('gulp-autoprefixer');
 var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 var autoprefixer = require('gulp-autoprefixer');
-// var sass = require('gulp-sass');
+var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
 var browserSync = require('browser-sync');
 var nodemon = require('gulp-nodemon');
 var nodeInspector = require('gulp-node-inspector');
 
-gulp.task('browser-sync', ['nodemon'], function () {
-    browserSync.init(null, {
-        proxy: "http://localhost:3000",
-        files: ["public/**/*.*"],
-        browser: "google-chrome",
-        port: 3001,
-        notify: false,
-    });
-});
+
 
 gulp.task('bs-reload', function () {
     browserSync.reload();
@@ -35,17 +27,17 @@ gulp.task('images', function () {
         .pipe(gulp.dest('dist/images/'));
 });
 
-// gulp.task('styles', function(){
-//   return gulp.src(['./public/styles/scss/**/*.scss', '!./public/styles/scss/partials/**'])
-//     .pipe(sourcemaps.init({ largeFile: true }))
-//     .pipe(sass({
-//       outputStyle: 'compressed',
-//       sourceMap: true
-//     }).on('error', sass.logError))
-//     .pipe(sourcemaps.write('./maps'))
-//     .pipe(gulp.dest('public/styles/css'))
-//     .pipe(browserSync.reload({stream:true}))
-// });
+gulp.task('styles', function(){
+  return gulp.src(['./public/styles/scss/**/*.scss', '!./public/styles/scss/partials/**'])
+    .pipe(sourcemaps.init({ largeFile: true }))
+    .pipe(sass({
+    //   outputStyle: 'compressed',
+    //   sourceMap: true
+    }).on('error', sass.logError))
+    // .pipe(sourcemaps.write('./maps'))
+    .pipe(gulp.dest('public/styles/css'))
+    .pipe(browserSync.reload({stream:true}))
+});
 
 gulp.task('scripts', function () {
     return gulp.src('public/js/partials/*.js')
@@ -67,22 +59,30 @@ gulp.task('nodemon', function () {
     nodemon({
         script: 'app.js',
         ext: 'js html',
-        ignore: 'public/img/*',
-        env: {
-            'NODE_ENV': 'development'
-        }
+        ignore: 'public/img/*'
+        // env: {
+        //     'NODE_ENV': 'development'
+        // }
         // , exec: 'node-inspector & node --debug'
     })
 });
-
+gulp.task('browser-sync', function () {
+    browserSync.init(null, {
+        proxy: "http://localhost:3000",
+        files: ["public/**/*.*"],
+        browser: "google-chrome",
+        port: 3001,
+        notify: false,
+    });
+});
 gulp.task('no-wifi', ['nodemon'], function () {
     gulp.watch("public/styles/scss/**/*.scss", ['styles']);
     gulp.watch("public/js/partials/*.js", ['scripts']);
     gulp.watch("views/**/*.ejs");
 });
 
-gulp.task('serve', ['browser-sync'], function () {
-    gulp.watch("public/main.css", ['prefix']);
+gulp.task('serve', ['nodemon', 'browser-sync'], function () {
+    gulp.watch("public/styles/scss/**/*.scss", ['styles']);
     gulp.watch("public/js/partials/*.js", ['scripts', 'bs-reload']);
     gulp.watch("views/**/*.html", ['bs-reload']);
     // gulp.watch(config.jade.watch, ['jade', browserSync.reload]);
