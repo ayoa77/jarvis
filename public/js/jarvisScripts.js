19e0c73c66b8d5ce37edb326af8a03319094dad3
Nathan <PaperNathan@gmail.com>
2018-01-07 01:16:26 +0800

    modal and css additions

--- public/js/jarvisScripts.js
+++ public/js/jarvisScripts.js
@@ -1,3 +1,13 @@
-$(document).ready(function(){ 
+$(document).ready(function() {
+  
+    $( '.signin' ).click(function() {
+      $( '.signup' ).removeClass('active');
+      $( '.signin' ).addClass('active');
+    });
     
-});
+    $( '.signup' ).click(function() {
+      $( '.signin' ).removeClass('active');
+      $( '.signup' ).addClass('active');
+    });
+    
+  });
