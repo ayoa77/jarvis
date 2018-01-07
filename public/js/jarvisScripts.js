67c7079575e169344239fc8035c87ac94609b9fc
Nathan <PaperNathan@gmail.com>
2018-01-08 01:19:46 +0800

    styling for major containers

--- public/js/jarvisScripts.js
+++ public/js/jarvisScripts.js
@@ -1,13 +1,13 @@
 $(document).ready(function() {
   
     $( '.signin' ).click(function() {
-      $( '.signup' ).removeClass('active');
-      $( '.signin' ).addClass('active');
+      $( '.signup' ).removeClass('activeMode');
+      $( '.signin' ).addClass('activeMode');
     });
     
     $( '.signup' ).click(function() {
-      $( '.signin' ).removeClass('active');
-      $( '.signup' ).addClass('active');
+      $( '.signin' ).removeClass('activeMode');
+      $( '.signup' ).addClass('activeMode');
     });
     
   });
