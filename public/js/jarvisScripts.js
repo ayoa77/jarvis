7070c9d7a5a56060b85606c6aa8c56237d1dc352
Nathan <PaperNathan@gmail.com>
2018-01-09 12:44:41 +0800

    slick slider, footer, advisor, etc...

--- public/js/jarvisScripts.js
+++ public/js/jarvisScripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+    initSlickSlider();
   
     $( '.signin' ).click(function() {
       $( '.signup' ).removeClass('activeMode');
@@ -9,5 +10,49 @@ $(document).ready(function() {
       $( '.signin' ).removeClass('activeMode');
       $( '.signup' ).addClass('activeMode');
     });
-    
-  });
+ });
+
+
+
+
+
+ function initSlickSlider() {
+
+    $('.center').slick({
+      centerMode: true,
+      centerPadding: '60px',
+      slidesToShow: 3,
+      responsive: [
+        {
+          breakpoint: 768,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '20px',
+            slidesToShow: 3
+          }
+        },
+        {
+          breakpoint: 480,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '20px',
+            slidesToShow: 1
+          }
+        },
+        {
+          breakpoint: 320,
+          settings: {
+            arrows: false,
+            centerMode: true,
+            centerPadding: '10px',
+            slidesToShow: 1
+          }
+        }
+      ]
+    });
+
+}
+
+ 
