97da347acc0f5452a9afac8f0d699c7297b3229c
Nathan <PaperNathan@gmail.com>
2018-01-31 15:22:51 +0800

    ajax error handling tests, timer styles, user page modal hookup

--- public/js/ajarx.js
+++ public/js/ajarx.js
@@ -31,7 +31,7 @@ $(document).ready(function () {
             data: 'starting up'
         })
         .done(function (data) {
-            console.log("ok");
+            console.log("AJAX READY.");
         });
     });
 
@@ -39,125 +39,86 @@ $(document).ready(function () {
     $(document).on('click', '#loginButton', function (ev) {
         ev.preventDefault();
         body = ($("#loginForm").serialize());
-        console.log(body)
+        // console.log(body)
         $.ajax({
             method: 'POST',
             url: `/login`,
             data: body
             })
             .done(function (data) {
-                console.log(data)
+                // console.log(data)
                 if (typeof data.redirect === 'string' || data.redirect instanceof String) {
-                    alert(JSON.stringify(data.message));
+                    // alert(JSON.stringify(data.message));
                     location.href = location.protocol + '//' + data.redirect
                 } else {
-                    alert(JSON.stringify(data));
+                    $('.error-handling').css('height', '100px');
+                    $('.error-handling').text(data);
+                    setTimeout(function() {
+                        $('.error-handling').css('height', '0px');
+                        $('.error-handling').text("");
+                    }, 3000)
+                    // alert(JSON.stringify(data));
                 }
             });
     });
 
-        //ajax register
-        $(document).on('click', '#registerButton', function(ev) {
-            ev.preventDefault();
-            body = ($("#registerForm").serialize());
-            console.log(body)
-                    $.ajax({
-                method: 'POST',
-                url: `/register`,
-                data: body
-                })
-            .done(function (data) {
-                console.log(data)
-                 if (typeof data.redirect === 'string' || data.redirect instanceof String) {
-                     console.log(data);
-                     console.log(location.protocol + '//' + data.redirect )
-                     alert(data.message);
-                    location.href = location.protocol + '//' + data.redirect 
-                 }else{
-
-                    alert(JSON.stringify(data));
-                    // location.reload();
-                }
-            });
-        });
-        
-        // send email for password reset
-        $(document).on('click', '#emailResetButton', function (ev) {
-            ev.preventDefault();
-            body = ($("#forgotPassForm").serialize());
-            console.log(body)
-            $.ajax({
-                method: 'POST',
-                url: `/emailresetpassword`,
-                data: body
-            })
-            .done(function (data) {
-                console.log(data)
-                if (typeof data.redirect === 'string' || data.redirect instanceof String) {
-                    alert(JSON.stringify(data.message));
-                    location.href = location.protocol + '//' + data.redirect;
-                    //location.reload
-                } else {
-                    alert(JSON.stringify(data));
-                };
-            });
-        })
-        
-        // Ajax reset password
-        $(document).on('click', '#passwordResetButton', function (ev) {
-            ev.preventDefault();
-            body = ($("#passwordResetForm").serialize());
-            console.log(body)
-            $.ajax({
-                method: 'POST',
-                url: `/resetpassword`,
-                data: body
-
-                })
-                .done(function (data) {
-                    console.log(data)
-                    if (typeof data.redirect === 'string' || data.redirect instanceof String) {
-                        alert(JSON.stringify(data.message));
-                        location.href = location.protocol + '//' + data.redirect
-                    } else if (typeof data.failure === 'string' || data.failure instanceof String) {
-                        alert(JSON.stringify(data.message));
-                        location.href = location.protocol + '//' + data.failure
-                    } else {
-                        alert(JSON.stringify(data));
-                    }
-                });
-        })
-
-        //ajax send verification email
-        $(document).on('click', '#verifyButton', function (ev) {
-            ev.preventDefault();
-                body = ($("#sendVerificationForm").serialize());
+    //ajax register
+    $(document).on('click', '#registerButton', function(ev) {
+        ev.preventDefault();
+        body = ($("#registerForm").serialize());
+        console.log(body)
                 $.ajax({
-                    method: 'POST',
-                    url: `//modal/email-verify`,
-                    data: body
-                    // contentType: "application/json",
-                    // dataType: "json"
-                })
-                .done(function (data) {
-                    if (typeof data.redirect === 'string' || data.redirect instanceof String) {
-                        alert(JSON.stringify(data.message));
-                        location.href = location.protocol + '//' + data.redirect
-                    } else {
-                        alert(JSON.stringify(data));
-                        //      location.reload();
-                    }
-                });
-            });
-                //ajax user editor
-        $(document).on('click', '#editUserButton', function (ev) {
-            ev.preventDefault();
-            body = ($("#editUserForm").serialize());
-            console.log(body)
-            $.ajax({
-                method: 'POST',
-                url: `/user`,
-                data: body
+            method: 'POST',
+            url: `/register`,
+            data: body
+            })
+        .done(function (data) {
+            console.log(data)
+                if (typeof data.redirect === 'string' || data.redirect instanceof String) {
+                    console.log(data);
+                    console.log(location.protocol + '//' + data.redirect )
+                    alert(data.message);
+                location.href = location.protocol + '//' + data.redirect 
+                }else{
+
+                alert(JSON.stringify(data));
+                // location.reload();
+            }
+        });
+    });
+        
+    // send email for password reset
+    $(document).on('click', '#emailResetButton', function (ev) {
+        ev.preventDefault();
+        body = ($("#forgotPassForm").serialize());
+        console.log(body)
+        $.ajax({
+            method: 'POST',
+            url: `/emailresetpassword`,
+            data: body
+        })
+        .done(function (data) {
+            console.log(data)
+            if (typeof data.redirect === 'string' || data.redirect instanceof String) {
+                alert(JSON.stringify(data.message));
+                location.href = location.protocol + '//' + data.redirect;
+                //location.reload
+            } else {
+                alert(JSON.stringify(data));
+            };
+        });
+    })
+        
+    // Ajax reset password
+    $(document).on('click', '#passwordResetButton', function (ev) {
+        ev.preventDefault();
+        body = ($("#passwordResetForm").serialize());
+        console.log(body)
+        $.ajax({
+            method: 'POST',
+            url: `/resetpassword`,
+            data: body
+
             })
             .done(function (data) {
                 console.log(data)
@@ -171,6 +132,55 @@ $(document).ready(function () {
                     alert(JSON.stringify(data));
                 }
             });
+    })
 
-        })   
+    //ajax send verification email
+    $(document).on('click', '#verifyButton', function (ev) {
+        ev.preventDefault();
+
+        body = ($("#sendVerificationForm").serialize());
+        $.ajax({
+            method: 'POST',
+            url: `//modal/email-verify`,
+            data: body
+            // contentType: "application/json",
+            // dataType: "json"
+        })
+
+        .done(function (data) {
+            if (typeof data.redirect === 'string' || data.redirect instanceof String) {
+                alert(JSON.stringify(data.message));
+                location.href = location.protocol + '//' + data.redirect
+            } else {
+                alert(JSON.stringify(data));
+                //      location.reload();
+            }
+        });
+    });
+
+
+    //ajax user editor
+    $(document).on('click', '#editUserButton', function (ev) {
+        ev.preventDefault();
+        body = ($("#editUserForm").serialize());
+        console.log(body)
+        $.ajax({
+            method: 'POST',
+            url: `/user`,
+            data: body
+        })
+        .done(function (data) {
+            console.log(data)
+            if (typeof data.redirect === 'string' || data.redirect instanceof String) {
+                alert(JSON.stringify(data.message));
+                location.href = location.protocol + '//' + data.redirect
+            } else if (typeof data.failure === 'string' || data.failure instanceof String) {
+                alert(JSON.stringify(data.message));
+                location.href = location.protocol + '//' + data.failure
+            } else {
+                alert(JSON.stringify(data));
+            }
+        });
+
+    })   
 });
---
+++ routes/indexAfterRoute.js
@@ -0,0 +1,30 @@
+var express = require('express');
+var router = express.Router();
+var mongoose = require('mongoose');
+var mailingListSchema = mongoose.model('mailingList', mailingListSchema);
+var Mailchimp = require('mailchimp-api-v3');
+var validator = require('validator');
+//DEV API
+var mailchimp = new Mailchimp('5e0b4a1b50b66ae689fb23e520122405-us17');
+//LIVE API
+// var mailchimp = new Mailchimp('mail chimp api from jarvis');
+var md5 = require('md5');
+
+langCheck = require('../middleware/langChecker.js');
+
+var csrf = require('csurf');
+var csrfProtection = csrf({ cookie: true });
+
+
+
+/* GET home page. */
+router.get('/after', csrfProtection,langCheck, function (req, res, next) {
+// console.log(req.session)
+  //  console.log(req.acceptsLanguages('en', 'zh-TW', 'zh', 'jp', 'kr'));
+  res.render('index-after', { title: 'Jarvis',
+  lang:lang,
+  sessionFlash: res.locals.sessionFlash, 
+  csrfToken: req.csrfToken() });
+});
+
+module.exports = router;
