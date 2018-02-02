d2fc69cc8c7c1ef51044f95c18e865f86faf4895
Nathan <PaperNathan@gmail.com>
2018-02-02 12:08:12 +0800

    User fixes, Commit Eth Modal, Ajax updates

--- public/js/ajarx.js
+++ public/js/ajarx.js
@@ -6,6 +6,7 @@ $(document).ready(function () {
         if (modal == "#modal=email-verify") { modalLoad("modal-email-verify"); }
         if (modal == "#modal=pass-reset") { modalLoad("modal-pass-reset"); }
         if (modal == "#modal=user-edit") { modalLoad("modal-user-edit"); }
+        if (modal == "#modal=commit-eth") {modalLoad("modal-commit-eth")};
         if (modal == "#modal=login") { modalLoad("modal-login"); }
         if (modal == "#modal=restricted-country") {modalLoad("modal-restricted-country")};
     });
@@ -19,6 +20,7 @@ $(document).ready(function () {
         if (modal == "#modal=email-verify") { modalLoad("modal-email-verify"); }
         if (modal == "#modal=pass-reset") {modalLoad("modal-pass-reset"); }
         if (modal == "#modal=user-edit") {modalLoad("modal-user-edit"); }
+        if (modal == "#modal=commit-eth") {modalLoad("modal-commit-eth")};
         if (modal == "#modal=login") {modalLoad("modal-login"); 
         if (modal == "#modal=restricted-country") { modalLoad("modal-restricted-country") };
     }}));
